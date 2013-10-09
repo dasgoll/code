05323e1260abcb5effed0377b8f97643f86234f8
dasgoll <jalal.araidah@gmail.com>
2013-10-09 16:39:37 -0700

    Update parse_library.py

--- python/xml_example/parse_library.py
+++ python/xml_example/parse_library.py
@@ -8,3 +8,9 @@ root = ElementTree.parse('library.xml')
 
 for b in root.findall('book'):
 	print b.findtext('title')
+	
+# to count numbers of books
+# count = 0 
+# for b in root.findall('book'):
+#	count += 1
+#print count
